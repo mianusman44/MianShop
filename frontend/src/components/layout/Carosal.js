cf033deeccf09a266a51cee4c527ad5088f8caab
Muhammad Usman <54813550+usmanshahzad-044@users.noreply.github.com>
2021-09-10 21:07:34 +0500

    Update Carosal.js

--- frontend/src/components/layout/Carosal.js
+++ frontend/src/components/layout/Carosal.js
@@ -15,44 +15,25 @@ const Carosal = () => {
 
 
 
-            <Carousel pause='hover' className='bg-dark' className='mt-2'>
+         
+            <Carousel pause='hover' className='bg-dark' className='mt-2' style={{ height: '600px', width: '100%' }}>
                 {products && products.map((product) => (
-                    <Carousel.Item key={product._id}>
+                    <Carousel.Item key={product._id} style={{ height: '600px', width: '100%' }}>
+
                         <Link to={`/product/${product._id}`}>
-                            <Image className="d-block w-100" src={product.images[0].url} alt={product.name} />
-                            <Carousel.Caption className='carousel-caption'>
-                                <h2>
+                            <Carousel.Caption className='carousel-caption' style={{ marginBottom: '100px' }}>
+                                <h2 >
                                     {product.name} (${product.price})
                                 </h2>
                             </Carousel.Caption>
+                            <Image className="d-block w-100 " src={product.images[0].url} alt={product.name} />
+
                         </Link>
                     </Carousel.Item>
                 ))}
             </Carousel>
 
-            {/* 
-            <div className={`carousel-item ${active}`}>
-                <img src={product.images[0].url} alt="Los Angeles" width="100%" height="500" />
-                <div className="carousel-caption">
-                    <h3>Los Angeles</h3>
-                    <p>We had such a great time in LA!</p>
-                </div>
-
-            </div> */}
-            {/* <div className="carousel-item">
-                            <img src="images/b.jpg" alt="Chicago" width="100%" height="500" />
-                            <div className="carousel-caption">
-                                <h3>Chicago</h3>
-                                <p>Thank you, Chicago!</p>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <img src="images/a.jpg" alt="New York" width="100%" height="500" />
-                            <div className="carousel-caption">
-                                <h3>New York</h3>
-                                <p>We love the Big Apple!</p>
-                            </div>
-                        </div> */}
+        
 
 
 
